49d40643fce0cba259a79f3a3db6ea96b93b97dc
Luky A M <luky.a.m1303@gmail.com>
2023-06-04 19:07:01 +0700

    Progress Minggu 14/15
    Commit 4 Juni

---
+++ VR GAME OBSTACLE JUMP/Assets/Script/PhysicsRig.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsRig : MonoBehaviour
+{
+    public Transform playerHead;
+    public Transform leftController;
+    public Transform rightController;
+
+    public ConfigurableJoint headJoint;
+    public ConfigurableJoint leftHandJoint;
+    public ConfigurableJoint rightHandJoint;
+
+    public CapsuleCollider bodyCollider;
+
+    public float bodyHeightMin = 0.5f;
+    public float bodyHeightMax = 2;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bodyCollider.height = Mathf.Clamp(playerHead.localPosition.y, bodyHeightMin, bodyHeightMax);
+        bodyCollider.center = new Vector3(playerHead.localPosition.x, bodyCollider.height / 2,
+            playerHead.localPosition.z);
+       
+        leftHandJoint.targetPosition = leftController.localPosition;
+        leftHandJoint.targetRotation = leftController.localRotation;
+
+        rightHandJoint.targetPosition = rightController.localPosition;
+        rightHandJoint.targetRotation = rightController.localRotation;
+
+        headJoint.targetPosition = playerHead.localPosition;
+
+    }
+}
